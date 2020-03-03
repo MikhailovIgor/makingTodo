82723c46c01ee7ae93b9f7394d9a4fd30a3f172f
MikhailovIgor <44831141+MikhailovIgor@users.noreply.github.com>
2020-03-03 15:27:55 +0300

    Add files via upload

---
+++ Todo native/main.js
@@ -0,0 +1,23 @@
+const input = document.getElementById('mainInput');
+const ulItems = document.getElementById('ulItems');
+
+let todoList = [];
+
+input.addEventListener('keydown', function(enter) {
+
+    if(enter.keyCode === 13) {
+        let oneMoreLi = document.createElement("li");
+
+        oneMoreLi.innerHTML = `<div class="item">
+                                    <input class="done" type="checkbox">
+                                        <label>
+                                            ${this.value}
+                                        </label>
+                                    <button class="delete">DEL</button>
+                                </div>`
+        ulItems.insertAdjacentElement("afterbegin", oneMoreLi);
+        todoList.unshift(oneMoreLi);
+        localStorage.setItem('todo', this.value);
+        this.value = '';
+    }
+});
